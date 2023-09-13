20d5530272f4d4f475e0b6cfa88590303fe19cdd
Matthew Landauer <mlandauer@gmail.com>
2023-09-13 11:09:03 +1000

    Get all applications up to a date back in time

--- scraper.rb
+++ scraper.rb
@@ -28,14 +28,28 @@ def scrape_api(url:, start:, length:)
       'address' => r['SiteAddress'],
       'description' => r['ReasonForPermit'],
       'info_url' => "#{url}/Public/ViewActivity.aspx?refid=#{URI.encode(council_reference)}",
-      'date_scraped' => Date.today.to_s,
-      'date_received' => Date.strptime(r['LodgedDate_STRING'], "%d-%b-%Y").to_s
+      'date_scraped' => Date.today,
+      'date_received' => Date.strptime(r['LodgedDate_STRING'], "%d-%b-%Y")
     }
   end  
 end
 
-# TODO: Add support for paging
-# TODO: Only get applications in a particular date range
-result = scrape_api(url: "https://eplanning.surfcoast.vic.gov.au", start: 0, length: 10)
+# Get all applications that were received on or after the start date
+def scrape_api_with_paging(url:, start_date:)
+  max_per_request = 10
 
-pp result
+  start = 0
+  loop do
+    results = scrape_api(url: url, start: start, length: max_per_request)
+    results.each do |r|
+      yield r if r["date_received"] >= start_date
+    end
+    break if results.any? { |r| r["date_received"] < start_date }
+    start += max_per_request
+  end
+end
+
+scrape_api_with_paging(url: "https://eplanning.surfcoast.vic.gov.au", start_date: Date.new(2023, 9, 1)) do |record|
+  # TODO: Convert dates back to strings
+  pp record
+end
